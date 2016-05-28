2df6776076f943bab94b69cbd8a4817ce5c97dea
Cheng-Lin Tsao <chenglin@starmobileinc.com>
2016-05-28 16:09:14 -0400

    Add a bar chart for cost comparison.

--- app/patient-cost-comparison.component.ts
+++ app/patient-cost-comparison.component.ts
@@ -4,34 +4,86 @@ import { RouteParams, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { Patient, CostComparison } from './patient';
 import { PatientService } from './patient.service';
 
+declare var jQuery:any;
+
 /**
  * Component that shows the details of a single patient.
  */
-@Component({
-	selector: 'patient-cost-comparison',
-	templateUrl: 'app/patient-cost-comparison.component.html',
-	directives: [ROUTER_DIRECTIVES]
-})
+ @Component({
+ 	selector: 'patient-cost-comparison',
+ 	templateUrl: 'app/patient-cost-comparison.component.html',
+ 	directives: [ROUTER_DIRECTIVES]
+ })
 
-export class PatientCostComparisonComponent implements OnInit {
-	patient: Patient;
-	costComparison: CostComparison;
-	keys: Array<string>;
-	
-	constructor(
-	  private patientService: PatientService,
-	  private routeParams: RouteParams) {
-	}
+ export class PatientCostComparisonComponent implements OnInit {
+ 	patient: Patient;
+ 	costComparison: CostComparison;
+ 	keys: Array<string>;
+ 	categories: Array<string>;
+ 	values: Array<number>;
 
-	ngOnInit() {
-		let id = +this.routeParams.get('id');
-		this.patient = this.patientService.getPatient(id);
-		this.costComparison = {};
-		this.keys = ['gender', 'city', 'country', 'company', 'job', 'race'];
-		this.keys.forEach(key => this.costComparison[key] = this.patientService.getAverageCost(key, this.patient[key]));
+ 	constructor(
+ 		private patientService: PatientService,
+ 		private routeParams: RouteParams) {
+ 	}
+
+ 	ngOnInit() {
+ 		let id = +this.routeParams.get('id');
+ 		this.patient = this.patientService.getPatient(id);
+ 		this.costComparison = {};
+ 		this.categories = [ 'My cost' ];
+ 		this.values = [ +this.patient.procedure.cost ];
+ 		this.keys = ['gender', 'city', 'country', 'company', 'job', 'race'];
+ 		this.keys.forEach(key => {
+ 			this.costComparison[key] = this.patientService.getAverageCost(key, this.patient[key]);
+ 			this.categories.push(this.patient[key] + ' (' + key + ')');
+ 			this.values.push(this.patientService.getAverageCost(key, this.patient[key]));
+ 		});
+ 	}
+
+ 	ngAfterViewInit() {
+ 		this.renderChart();
+ 	}
+
+ 	renderChart(){
+ 		jQuery('#container').highcharts({
+ 			chart: {
+ 				type: 'bar'
+ 			},
+ 			title: {
+ 				text: 'Cost Comparison for ' + this.patient.first_name + ' ' + this.patient.last_name
+ 			},
+ 			xAxis: {
+ 				categories: this.categories
+ 			},
+ 			yAxis: {
+ 				min: 0,
+ 				title: {
+ 					text: 'Average procedure cost (US dollar)',
+ 					align: 'high'
+ 				},
+ 				labels: {
+ 					overflow: 'justify'
+ 				}
+ 			},
+ 			plotOptions: {
+ 				bar: {
+ 					dataLabels: {
+ 						enabled: true
+ 					}
+ 				}
+ 			},
+ 			credits: {
+ 				enabled: false
+ 			},
+ 			series: [{
+ 				name: 'Cost',
+ 				data: this.values
+ 			}]
+ 		});
 	}
 
 	goBack() {
-	  window.history.back();
+		window.history.back();
 	}
 }
