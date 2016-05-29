1ed332aef84b0890be557fb52f930fb5270c39c3
Cheng-Lin Tsao <chenglin@starmobileinc.com>
2016-05-28 21:30:22 -0400

    Add autocomplete to select a patient.

--- app/patients.component.ts
+++ app/patients.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router-deprecated';
 import { Patient } from './patient';
 import { PatientService } from './patient.service';
 
+declare var jQuery:any;
+
 /**
  * Component that lists patients for selecting a patient to view details.
  */
@@ -11,8 +13,11 @@ import { PatientService } from './patient.service';
   selector: 'patients',
   template:`
     <h2>Patient Selection</h2>
+    <h3>Enter patient name</h3>
+    <input id="patient-name">
+    <h3>Or pick one of the first 20 patients</h3>
     <ul class="patients">
-      <li *ngFor="let patient of patients" (click)="gotoPatient(patient)">
+      <li *ngFor="let patient of patients" (click)="gotoPatient(patient.id)">
         <span class="badge">{{patient.id}}</span> {{patient.first_name}}  {{patient.last_name}}
       </li>
     </ul>
@@ -21,7 +26,6 @@ import { PatientService } from './patient.service';
 
 export class PatientsComponent implements OnInit {
   patients: Patient[];
-  selectedPatient: Patient;
 
   constructor(
     private router: Router,
@@ -29,16 +33,26 @@ export class PatientsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getPatients();
+    this.patients = this.patientService.getPatients();
+    var tags = [];
+    this.patients.forEach(function(patient){
+      tags.push({
+        value: patient.id,
+        label: patient.first_name + ' ' + patient.last_name
+      });
+    });
+    var _this = this;
+    $("#patient-name").autocomplete({
+      source: tags,
+      select: function( event, ui ) {
+        _this.gotoPatient(ui.item.value);
+      }
+    });
+    this.patients = this.patients.slice(0, 20);
   }
 
-  getPatients() {
-    //this.patientService.getPatients().then(patients => this.patients = patients.slice(0,20));
-    this.patients = this.patientService.getPatients().slice(0, 20);
-  }
-
-  gotoPatient(patient: Patient) {
-    let link = ['Patient', {id: patient.id}];
+  gotoPatient(id: number) {
+    let link = ['Patient', {id: id}];
     this.router.navigate(link);
   }
 }
